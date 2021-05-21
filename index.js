01046ecf02a1ec1ca88200780c1d6201744fe4a2
Gabriel Fragoso <47670668+Romulus-II@users.noreply.github.com>
2021-05-21 18:49:13 -0500

    Added More Documentation

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
+const bodyParser = require('body-parser');
 var express = require('express');
+const path = require('path');
 const app = express();
 const serv = require('http').Server(app);
 
+// Set up view engine
+app.set('views', path.join(__dirname, 'client/views'));
+app.set('view engine','ejs');
 
 
-console.log('Hello');
+app.use('/client', express.static(__dirname + '/client'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+var routes = require('./routes/index.js');
+
+app.use('/', routes);
+
+
+serv.listen(5000);
+console.log("Server started.");
---
+++ sprint-log.md
@@ -0,0 +1,16 @@
+## Sprint 1: Setup & Memory (Single Player)
+
+Start Date: 5/19/21
+End Date: 6/02/21
+
+### To-Do:
+
+- Create js script for generating "table"
+- Create html card framework
+- Create hiragana json file
+
+### In Progress:
+
+
+### Done:
+- Set up node.js project
